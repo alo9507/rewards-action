6326b870304b98a2b1eaae5541ce6e9fdc79ceee
mktcode <kontakt@markus-kottlaender.de>
2021-05-14 01:56:12 +0200

    test sending tx

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const Web3 = require('web3');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 try {
   const rewards = core.getInput('rewards');
@@ -10,7 +11,22 @@ try {
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  const web3 = new Web3(rpcNode)
+  console.log(payload)
+  const web3 = new Web3(new HDWalletProvider(seedPhrase, rpcNode))
+
+  web3.eth.sendTransaction({
+    from: '0xA56d9e73f98212e56A2eFb00c9F47d1da64937ee',
+    to: '0x0cE5CD28e4CD4b3a4def3c9eE461809b2c5ee9E6',
+    value: '1000000000000000'
+  })
+  .on('transactionHash', function(hash){
+      console.log(hash)
+  })
+  .on('receipt', function(receipt){
+      console.log(receipt)
+  })
+  .on('confirmation', function(confirmationNumber, receipt){ })
+  .on('error', function(e) { throw e });
 
   // TODO:
   // - find issue author's eth address (via Octobay's UserAddressStorage or on GitHub (repo, description, gist, we'll see)
