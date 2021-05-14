f4f84892a412b8be799b11fb40c3441e6a796291
mktcode <kontakt@markus-kottlaender.de>
2021-05-14 03:10:17 +0200

    wrapping in async function

--- index.js
+++ index.js
@@ -5,28 +5,25 @@ const HDWalletProvider = require('@truffle/hdwallet-provider')
 const rewards = require('./.octobay.json')
 console.log(rewards)
 
-try {
-  const seedPhrase = core.getInput('seed-phrase')
-  const rpcNode = core.getInput('rpc-node')
+(async () => {
+  try {
+    const seedPhrase = core.getInput('seed-phrase')
+    const rpcNode = core.getInput('rpc-node')
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  const web3 = new Web3(new HDWalletProvider(seedPhrase, rpcNode))
-  web3.eth.getAccounts().then(accounts => console.log(accounts))
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    const web3 = new Web3(new HDWalletProvider(seedPhrase, rpcNode))
+    const accounts = web3.eth.getAccounts()
+    console.log(accounts)
 
-  web3.eth.sendTransaction({
-    from: '0xA56d9e73f98212e56A2eFb00c9F47d1da64937ee',
-    to: '0x0cE5CD28e4CD4b3a4def3c9eE461809b2c5ee9E6',
-    value: '1000000000000000'
-  })
-  .on('error', function(e) { throw e })
+    const tx = await web3.eth.sendTransaction({
+      from: '0xA56d9e73f98212e56A2eFb00c9F47d1da64937ee',
+      to: '0x0cE5CD28e4CD4b3a4def3c9eE461809b2c5ee9E6',
+      value: '1000000000000000'
+    })
 
-  // TODO:
-  // - find issue author's eth address (via Octobay's UserAddressStorage or on GitHub (repo, description, gist, we'll see)
-  // - send transaction
-  let tx
-
-  core.setOutput("tx", tx)
-} catch (error) {
-  core.setFailed(error.message)
-}
+    core.setOutput("tx", tx)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+})()
